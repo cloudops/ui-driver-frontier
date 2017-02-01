93b09a0a290a307e886cf8867670fdd1ddca646b
Adam Carruthers <acarruthers@cloudops.com>
2017-02-01 17:08:23 -0500

    List environments dynamically

--- component/component.js
+++ component/component.js
@@ -43,10 +43,64 @@ define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'emb
     },
 
     firstPage: true,
+    servicesByEnvironmentName: {},
     actions: {
       nextPage: function () {
-        this.set('firstPage', false);
+        this.apiCall('/environments', function (environments) {
+          if (environments.errors) {
+            this.set('errors', environments.errors.map(function (err) {
+              return err.message;
+            }));
+            return;
+          }
+
+          var envs = environments.data.filter(function (env) {
+            return env.serviceConnection.type === 'CloudCA';
+          });
+
+          this.set('servicesByEnvironmentName', envs.reduce(function (m, e) {
+            m[e.name] = e.serviceConnection.serviceCode;
+            return m;
+          }, {}));
+
+          this.set('environmentOptions', envs
+            .map(function (env) {
+              return {
+                name: env.serviceConnection.serviceCode + ' / ' + env.name,
+                value: env.id
+              };
+            }));
+          this.set('firstPage', false);
+        }.bind(this));
       }
+    },
+
+    // TODO: make these computed properties? ie _ember.default.computed(...)
+    networkOptions: _ember.default.computed('', function () {
+      // TODO: return the networks in response to an env change (either using ember.computed or like environmentNameChanged below)
+      return [{name: 'holy moly batman', value: 'asdfadf'}];
+    }),
+    templateOptions: [],
+    environmentOptions: [],
+    diskOfferingOptions: [],
+    computeOfferingOptions: [],
+
+    environmentNameChanged: function () {
+      alert('adfasdf');
+      var serviceCode = this.servicesByEnvironmentName[this.get('model.%%DRIVERNAME%%Config.environmentName')];
+      this.set('model.%%DRIVERNAME%%Config.serviceCode', serviceCode);
+    }.observes('model.%%DRIVERNAME%%Config.environmentName'),
+
+    apiCall: function (endpoint, callback) {
+      var url = this.get('model.%%DRIVERNAME%%Config.apiUrl') + endpoint,
+          xhr = new XMLHttpRequest();
+      xhr.addEventListener('load', function () {
+        callback(JSON.parse(this.responseText));
+      });
+      xhr.open('get', url, true);
+      xhr.setRequestHeader('MC-Api-Key', this.get('model.%%DRIVERNAME%%Config.apiKey'));
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send();
     }
   });
 });
